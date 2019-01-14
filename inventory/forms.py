a5a1eec058d2a9a21decddbcbcd9f0586d2278e2
BoYanZh <32470225+BoYanZh@users.noreply.github.com>
2019-01-14 15:50:12 +0800

    Fix: unit should not be forcibly required

--- inventory/forms.py
+++ inventory/forms.py
@@ -7,9 +7,9 @@ class AddItemForm(forms.Form):
         max_length=128,
         widget=forms.TextInput(attrs={'class': 'form-control'}))
     quantity = forms.DecimalField(
-        label="数量",
-        widget=forms.TextInput(attrs={'class': 'form-control'}))
+        label="数量", widget=forms.TextInput(attrs={'class': 'form-control'}))
     unit = forms.CharField(
         label="单位",
         max_length=32,
+        required=False,
         widget=forms.TextInput(attrs={'class': 'form-control'}))
