e02ce506713a4978060c0ed7ad3f350c01a398a3
BoYanZh <32470225+BoYanZh@users.noreply.github.com>
2019-01-21 12:40:11 +0800

    Fix: add item min quantity

--- inventory/forms.py
+++ inventory/forms.py
@@ -5,23 +5,26 @@ class AddItemForm(forms.Form):
     name = forms.CharField(
         label="名称",
         max_length=128,
-        widget=forms.TextInput(attrs={'class': 'form-control'}))
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
     quantity = forms.DecimalField(
         label="数量",
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'type': 'number',
-            'step': '0.01',
-            'value': '1',
-            'min': '0.01',
-        }),
-        min_value=1,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'type': 'number',
+                'step': '0.01',
+                'value': '1',
+                'min': '0.01',
+            }),
+        min_value=0.01,
     )
     unit = forms.CharField(
         label="单位",
         max_length=32,
         required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'}))
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
 
 
 class UseItemForm(forms.Form):
@@ -34,4 +37,5 @@ class UseItemForm(forms.Form):
                 'step': '0.01',
                 'min': '0.01',
                 'value': '1',
-            }))
+            }),
+    )
