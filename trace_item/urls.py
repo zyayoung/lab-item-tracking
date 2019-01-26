4a14f540b198eedbf425297e1f464aee1cadc2f3
zyayoung <1328410180@qq.com>
2019-01-26 13:08:43 +0800

    Trace Location Available Now!

--- trace_item/urls.py
+++ trace_item/urls.py
@@ -5,5 +5,6 @@ from trace_item import views
 app_name = 'trace_item'
 
 urlpatterns = [
-    url(r'^item/(?P<id>\d+)/trace/$', views.TraceItemView.as_view(), name='trace'),
+    url(r'^item/(?P<id>\d+)/trace/$', views.TraceItemView.as_view(), name='traceItem'),
+    url(r'^location/(?P<id>\d+)/trace/$', views.TraceLocationView.as_view(), name='traceLoc'),
 ]
--- trace_item/views.py
+++ trace_item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 from trace_item.models import ItemLog
-from inventory.utils import get_my_item
+from inventory.utils import get_my_item, get_my_loc
 from login.models import User as myUser
 
 
@@ -12,3 +12,11 @@ class TraceItemView(generic.View):
         item = get_my_item(tmp_user, kwargs.get('id'))
         logs = ItemLog.objects.filter(item=item)
         return render(request, 'trace_item/trace_item.html', locals())
+
+
+class TraceLocationView(generic.View):
+    def get(self, request, *args, **kwargs):
+        tmp_user = myUser.objects.get(id=request.session.get('user_id'))
+        loc = get_my_loc(tmp_user, kwargs.get('id'))
+        logs = ItemLog.objects.filter(location_to=loc) | ItemLog.objects.filter(location_from=loc)
+        return render(request, 'trace_item/trace_location.html', locals())
