74b6b35ed2c5d2676f8ca5b146fa030a92db5442
zyayoung <1328410180@qq.com>
2019-01-26 11:52:28 +0800

    Update admin.py

--- inventory/admin.py
+++ inventory/admin.py
@@ -6,6 +6,14 @@ class LocationPermissionApplicationAdmin(admin.ModelAdmin):
     list_display = ('location', 'applicant', 'time', 'auditor')
 
 
-admin.site.register(Item)
-admin.site.register(Location)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'unit', 'location', 'owner', 'is_public', 'update_time')
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'path', 'is_public', 'allowed_users_summary')
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(LocationPermissionApplication, LocationPermissionApplicationAdmin)
