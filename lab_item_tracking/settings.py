b60fa96eefbbea28328a963709dd967c074c42d8
BoYanZh <32470225+BoYanZh@users.noreply.github.com>
2019-01-29 10:33:47 +0800

    Fix: static file loading

--- lab_item_tracking/settings.py
+++ lab_item_tracking/settings.py
@@ -130,8 +130,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+if not DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Profiling
 SILKY_INTERCEPT_PERCENT = 0
