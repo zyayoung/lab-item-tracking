7b848f29f1001fb661664b1ebe9af0f70b42915c
Zhang Yuang <1328410180@qq.com>
2019-01-29 12:03:13 +0800

    skip auto deploy

--- lab_item_tracking/settings.py
+++ lab_item_tracking/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'lab_item_tracking.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'lab',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': 'localhost',
+    'PORT': '54321',
+  }
 }
 
 # Password validation
