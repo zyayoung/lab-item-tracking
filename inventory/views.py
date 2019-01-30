5513d641042c5b7045a1b25db9ffebf7aa4c295a
Zhang Yuang <1328410180@qq.com>
2019-01-30 23:30:45 +0800

    Fix: last commit

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-sfrom django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
