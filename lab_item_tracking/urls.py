e9d99a7c7c6a165e95e295beb8e86eeae23d9e28
zya <1328410180@qq.com>
2019-01-31 21:21:08 +0800

    uwsgi

--- lab_item_tracking/urls.py
+++ lab_item_tracking/urls.py
@@ -31,6 +31,7 @@ def ssl_certification(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^admin/uwsgi/', include('django_uwsgi.urls')),
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}, name='static'),
     url(r'^captcha/', include('captcha.urls')),
