5e52728c9afe25665709965a402e96e07cff4763
Zhang Yuang <1328410180@qq.com>
2019-02-13 10:00:31 +0800

    Fix: English

--- inventory/views.py
+++ inventory/views.py
@@ -10,6 +10,7 @@ from log.utils import *
 import re
 
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext
 from inventory.models import Item, Location, LocationPermissionApplication, ItemTemplate
 from login.models import User as myUser
 from personal.utils import get_others_request_list
@@ -246,7 +247,7 @@ class ItemView(generic.View):
                 }))
         relation_info = {}
         for key, values in item.related_items.items():
-            relation_info[_('作为以下') + key.replace('__', _('的'))] = get_my_list(
+            relation_info[gettext('作为以下') + key.replace('__', gettext('的'))] = get_my_list(
                 tmp_user, Item.objects.filter(id__in=values))
         del_permission = item.del_permission(tmp_user)
         unlink_permission = item.unlink_permission(tmp_user)
